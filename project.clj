0e9e57363ccb0713d083fa1bc60fd408f06d547c
Ruiyun Wen <ruiyun.wen@gmail.com>
2012-04-22 09:24:57 +0800

    try to add autodoc support

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [javax.sip/jain-sip-api "1.2.1.4"]
                  [javax.sip/jain-sip-ri "1.2.166"]
-                 [log4j/log4j "1.2.16"]])
+                 [log4j/log4j "1.2.16"]]
+  :dev-dependencies [[lein-autodoc "0.9.0"]]
+  :autodoc {:name "cljain", :page-title "cljain API Documentation"})
