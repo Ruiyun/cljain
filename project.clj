6ffa31bb7d552570f935ecf96a7506040ab13e53
Ruiyun <ruiyun.wen@gmail.com>
2013-01-08 10:55:10 +0800

    upgrade to lein2

--- project.clj
+++ project.clj
@@ -1,10 +1,15 @@
-(defproject cljain "0.4.0-SNAPSHOT"
-  :description "Use JAIN-SIP by Clojure way."
+(defproject cljain "0.5.0-SNAPSHOT"
+  :description "Enjoy JAIN-SIP with Clojure taste."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [ruiyun/tools.timer "1.0.0-SNAPSHOT"]
+                 [ruiyun/tools.timer "1.0.0"]
                  [javax.sip/jain-sip-api "1.2.1.4"]
-                 [javax.sip/jain-sip-ri "1.2.166"]
-                 [log4j/log4j "1.2.16"]]
-  :dev-dependencies [[lein-autodoc "0.9.0"]]
-  :autodoc {:name "cljain", :page-title "cljain API Documentation"})
+                 [javax.sip/jain-sip-ri "1.2.166"]]
+  :lein-release {:deploy-via :clojars}
+  :warn-on-reflection true
+  :min-lein-version "2.0.0"
+  :url "https://github.com/Ruiyun/cljain"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-api "1.6.4"]]
+                   :plugins [[codox "0.6.4"]]}})
